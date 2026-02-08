19cee598810625e7e3589526ad33d59d44e1fdfe
mariete <mariohernandez2112003@gmail.com>
2026-02-08 14:11:58 +0100

    mario
    Signed-off-by: mariete <mariohernandez2112003@gmail.com>

---
+++ practicaExamen/src/practicaExamen/PracticaExamen.java
@@ -0,0 +1,48 @@
+package practicaExamen;
+
+import java.util.Scanner;
+
+public class PracticaExamen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	String cadena;
+	char letra1='a';
+	int entero=0;
+	
+	System.out.println("Dime cadeana");	
+	cadena=sc.nextLine();
+	
+	
+	for(int contador=cadena.length()-1;contador>=0;contador--) {
+		
+		letra1=cadena.charAt(contador);
+		System.out.println(letra1);
+		
+		
+		
+		
+		
+	}
+	
+
+	
+	
+		
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+	
+
+
